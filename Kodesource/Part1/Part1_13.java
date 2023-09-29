789d5f555db74d1938e5d3734f5f010ebab60c00
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-09-29 08:17:21 +0300

    save 29/09/23

--- Kodesource/Part1/Part1_13.java
+++ Kodesource/Part1/Part1_13.java
@@ -1,11 +1,15 @@
 package javarush.Kodesource;
 /*
-13. Напишите Java-программу для печати площади и периметра прямоугольника. Перейти к редактору
+13. Напишите Java-программу для печати площади и периметра прямоугольника.
 Тестовые данные:
 Ширина = 5,5 Высота = 8,5
  */
 public class Part1_13 {
     public static void main(String[] args) {
+        double width = 5.5;
+        double height = 8.5;
+        System.out.println("периметр прямоугольника" + "=" + (width + height)*2);
+        System.out.println("площадь прямоугольника" + "=" + width * height);
 
     }
 }
