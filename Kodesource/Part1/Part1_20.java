4394eef52ef04e1d61c49a110d3f47595b0eb8b3
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-09-29 15:38:19 +0300

    save 29/09/23

---
+++ Kodesource/Part1/Part1_20.java
@@ -0,0 +1,15 @@
+package javarush.Kodesource;
+/*
+20. Напишите Java-программу для преобразования десятичного числа в шестнадцатеричное число.
+Входные данные:
+Введите десятичное число: 15
+Ожидаемый результат
+
+ Шестнадцатеричное число: F
+ */
+public class Part1_20 {
+    public static void main(String[] args) {
+        int a = 15;
+        System.out.println(Integer.toHexString(15));
+    }
+}
