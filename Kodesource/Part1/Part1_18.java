18ed385893dc4b9c952d77755e81f6bb337e9cf9
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-09-29 10:31:23 +0300

    save 29/09/23

---
+++ Kodesource/Part1/Part1_18.java
@@ -0,0 +1,37 @@
+package javarush.Kodesource;
+
+import java.util.Scanner;
+
+/*
+18. Напишите программу на Java для умножения двух двоичных чисел.
+Входные данные:
+Введите первое двоичное число: 10
+Введите второе двоичное число: 11
+Ожидаемый результат
+
+ Произведение двух двоичных чисел: 110
+ */
+public class Part1_18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое двоичное число:");
+        int num1 = scanner.nextInt();
+        System.out.println("Введите второе двоичное число:");
+        int num2 = scanner.nextInt();
+
+        //считаем произведение двух чисел
+        int sum = num1 * num2;
+
+        //Считаем номер старшего значащего бита суммы
+        int n = 0;
+        while (sum != 0) {
+            sum = sum / 2;
+            n++;
+        }
+        System.out.println(n);
+
+        //переводим в двоичную систему 0 и 1
+        String bin = Integer.toBinaryString(n);
+        System.out.println(bin);
+    }
+}
---
+++ Kodesource/Part1/Part1_19.java
@@ -0,0 +1,16 @@
+package javarush.Kodesource;
+/*
+19. Напишите программу на Java для преобразования десятичного числа в двоичное число. Перейти к редактору
+Входные данные:
+Введите десятичное число: 5
+Ожидаемый результат
+
+ Двоичный номер: 101
+ */
+public class Part1_19 {
+    public static void main(String[] args) {
+        int a = 5;
+        String bin = Integer.toBinaryString(5);
+        System.out.println(bin);
+    }
+}
