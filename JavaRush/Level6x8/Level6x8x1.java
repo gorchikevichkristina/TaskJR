040c10ba6678fe0b899eed543693cfc31cd736a1
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-10-09 22:16:11 +0300

    save 09/10/23

--- JavaRush/Level6x8/Level6x8x1.java
+++ JavaRush/Level6x8/Level6x8x1.java
@@ -1,8 +1,22 @@
 package javarush.Level6x8;
+
+import java.util.Arrays;
+
 //заполняем массив
+/*
+1, если длина массива четная то его первую половину заполняем valueStart
+2. если нечетная то большую част заполнить valueStart
+ */
 public class Level6x8x1 {
+    public static int[] array = new int[20];
+    public static int valueStart = 10;
+    public static int valueEnd = 13;
+
     public static void main(String[] args) {
-
-
+        //напишите тут ваш код
+        int firstPart = array.length % 2 == 0 ? array.length / 2 : array.length / 2 + 1;
+        Arrays.fill(array, 0, firstPart, valueStart);
+        Arrays.fill(array, firstPart, array.length, valueEnd);
+        System.out.println(Arrays.toString(array));
     }
 }
--- JavaRush/Level6x8/Level6x8x2.java
+++ JavaRush/Level6x8/Level6x8x2.java
@@ -1,7 +1,16 @@
 package javarush.Level6x8;
+
+import java.util.Arrays;
+
 //делим массив
 public class Level6x8x2 {
+    public static int[][] result = new int[2][];
+    public static int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; //поделить на 2 подмассива
     public static void main(String[] args) {
+        //напишите тут ваш код
+
+        System.out.println(Arrays.deepToString(result));
+
 
     }
 }
