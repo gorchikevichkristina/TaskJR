fdc58f168d28137252fcc6ac7ca1d92c8a8debb7
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-10-09 19:18:19 +0300

    save 09/10/23

--- JavaRush/Level6x6/Level6x6x3.java
+++ JavaRush/Level6x6/Level6x6x3.java
@@ -1,7 +1,18 @@
 package javarush.Level6x6;
+
+import java.util.Arrays;
+
 //создаем мультимассив
 public class Level6x6x3 {
     public static void main(String[] args) {
-
+        int[][][] multiArray = new int[][][]{{{4, 8, 15}, {16}}, {{23, 42}, {}}, {{1}, {2}, {3}, {4, 5}}};
+        //System.out.println(Arrays.deepToString(multiArray));
+        for (int i = 0; i < multiArray.length; i++) {
+            for (int j = 0; j < multiArray[i].length; j++) {
+                for (int k = 0; k < multiArray[i][j].length; k++) {
+                    System.out.println(multiArray[i][j][k]);
+                }
+            }
+        }
     }
 }
