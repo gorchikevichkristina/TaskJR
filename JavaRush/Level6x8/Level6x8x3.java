e37fc2bb18c8752da8e847b399bd3c40e56e31bf
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-10-10 18:30:50 +0300

    save 10/10/23

--- JavaRush/Level6x8/Level6x8x3.java
+++ JavaRush/Level6x8/Level6x8x3.java
@@ -1,7 +1,14 @@
 package javarush.Level6x8;
+
+import java.util.Arrays;
+
 //сортировка массива
 public class Level6x8x3 {
-    public static void main(String[] args) {
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Arrays.sort(array); //метод для сортировки
+        System.out.println(Arrays.toString(array));
     }
 }
--- JavaRush/Level6x8/Level6x8x4.java
+++ JavaRush/Level6x8/Level6x8x4.java
@@ -1,7 +1,15 @@
 package javarush.Level6x8;
+
+import java.util.Arrays;
+
 //есть ли кто
 public class Level6x8x4 {
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    public static int element = 0;
     public static void main(String[] args) {
-
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+        int in = Arrays.binarySearch(array,element);
+        System.out.println(in);
     }
 }
