d278de56f6a501f4974a429216e332dc163dac2e
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-10-12 14:52:15 +0300

    save 12/10/23

--- JavaRush/Level6x8/Level6x8x4.java
+++ JavaRush/Level6x8/Level6x8x4.java
@@ -5,11 +5,17 @@ import java.util.Arrays;
 //есть ли кто
 public class Level6x8x4 {
     public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    public static int element = 0;
+    public static int element = 5;
+
     public static void main(String[] args) {
-        Arrays.sort(array);
-        System.out.println(Arrays.toString(array));
-        int in = Arrays.binarySearch(array,element);
+        int [] array2 = Arrays.copyOf(array, array.length);
+        Arrays.sort(array2);
+        int in = Arrays.binarySearch(array, element);
         System.out.println(in);
+        if (in >= element) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
---
+++ JavaRush/Level7x1/Level7x1x2.java
@@ -0,0 +1,7 @@
+package javarush.Level7x1;
+
+public class Level7x1x2 {
+    public static void main(String[] args) {
+
+    }
+}
