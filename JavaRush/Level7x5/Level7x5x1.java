424f4a53f094097ab32f6973194402a5a571d379
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-10-26 20:21:07 +0300

    save 26/10/23

--- JavaRush/Level7x5/Level7x5x1.java
+++ JavaRush/Level7x5/Level7x5x1.java
@@ -1,4 +1,25 @@
 package javarush.Level7x5;
 
 public class Level7x5x1 {
+
+/*
+У всего есть корень
+*/
+
+    public static void main(String[] args) {
+        int[] array = {15, 64, 9, 51, 42};
+        printSqrt(array);
+    }
+
+    public static void printSqrt(int[] array) {
+        String elementSqrt = "Корень квадратный для числа ";
+        for (int i = 0; i < array.length; i++) {
+            int element = array[i];
+            double elementSqrtNum = Math.sqrt(element);
+            System.out.println(elementSqrt + element + " равен " + elementSqrtNum);
+        }
+    }
 }
+
+
+
--- JavaRush/Level7x5/Level7x5x2.java
+++ JavaRush/Level7x5/Level7x5x2.java
@@ -1,4 +1,23 @@
 package javarush.Level7x5;
 
 public class Level7x5x2 {
+
+    /*
+    Города-гиганты
+    */
+    public static String city = "Токио";
+    public static double population = 34.5;
+
+    public static void main(String[] args) {
+        printCityPopulation("Джакарта", 25.3);
+        printCityPopulation("Сеул", 25.2);
+        printCityPopulation("Дели", 23.1);
+        printCityPopulation("Нью-Йорк", 21.6);
+    }
+
+    public static void printCityPopulation(String city, double population) {
+        System.out.println("Население города " + city + " составляет " + population + " млн человек.");
+        System.out.println("В то время как в самом густонаселенном городе " + Level7x5x2.city + " население составляет " + Level7x5x2.population + " млн человек.");
+    }
 }
+
